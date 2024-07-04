3027cb3bea5322bccc1a8e802dcd347e02d5857d
Isabel Klint <isabel.klint@gmail.com>
2024-07-04 16:33:06 -0600

    better header

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,13 +16,13 @@ const Header = () => {
           <img src={affiliateLogos} alt="Affiliate Logos" className="h-10" />
         </div>
         <div className="flex flex-row flex-wrap justify-center md:flex-row items-center md:space-x-4 text-uppercase">
-          <Link to="/" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0">{t('home').toUpperCase()}</Link>
-          <Link to="/academics" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0">{t('academics').toUpperCase()}</Link>
-          <Link to="/registration" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0">{t('register').toUpperCase()}</Link>
-          <Link to="/instructors" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0">{t('instructors').toUpperCase()}</Link>
-          <Link to="/history" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0">{t('about').toUpperCase()}</Link>
-          <Link to="/alumni" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0">{t('alumni').toUpperCase()}</Link>
-          <Link to="/contact" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0">{t('contact.title').toUpperCase()}</Link>
+          <Link to="/" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0 px-2">{t('home').toUpperCase()}</Link>
+          <Link to="/academics" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0 px-2">{t('academics').toUpperCase()}</Link>
+          <Link to="/registration" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0 px-2">{t('register').toUpperCase()}</Link>
+          <Link to="/instructors" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0 px-2">{t('instructors').toUpperCase()}</Link>
+          <Link to="/history" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0 px-2">{t('about').toUpperCase()}</Link>
+          <Link to="/alumni" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0 px-2">{t('alumni').toUpperCase()}</Link>
+          <Link to="/contact" className="text-[#1A2534] hover:text-blue-500 mb-2 md:mb-0 px-2">{t('contact.title').toUpperCase()}</Link>
           <LanguageSwitcher />
         </div>
       </nav>
